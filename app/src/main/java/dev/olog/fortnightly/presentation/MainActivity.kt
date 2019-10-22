8685ac5709b09019173ec9b7c0f2a312b1fd1055
Eugeniu Olog <eugeniu.olog@gmail.com>
2019-10-22 16:35:46 +0200

    initial commit

---
+++ app/src/main/java/dev/olog/fortnightly/presentation/MainActivity.kt
@@ -0,0 +1,13 @@
+package dev.olog.fortnightly.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.olog.fortnightly.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
