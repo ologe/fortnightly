e35c4112fc238cf60156c9170b6c250c12045b02
Eugeniu Olog <eugeniu.olog@gmail.com>
2019-10-22 22:48:34 +0200

    fixes

--- app/src/main/java/dev/olog/fortnightly/data/ArticleService.kt
+++ app/src/main/java/dev/olog/fortnightly/data/ArticleService.kt
@@ -1,12 +1,13 @@
 package dev.olog.fortnightly.data
 
+import dev.olog.fortnightly.BuildConfig
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface ArticleService {
 
-    @GET("/svc/topstories/v2/{section}.json?api-key=2voMAqUaP5Dg6ArIx1Z58vlbxjhzb51K&page=1")
+    @GET("/svc/topstories/v2/{section}.json?api-key=${BuildConfig.NY_TIMES_KEY}")
     suspend fun fetchTopStories(@Path("section") section: String = "home"): Response<TopArticlesRemote>
 
 }
